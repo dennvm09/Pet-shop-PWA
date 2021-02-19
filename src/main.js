d632598cf38b21996334bd4c1ed19749731e1c66
dennvm09 <valenmosquera1999@gmail.com>
2021-02-18 20:49:00 -0500

    lerning vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import routes from './plugins/router'; /**importo el router */
+
+Vue.use(VueRouter);
+const router = new VueRouter ({routes}); /**creo el objeto router */
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/pages/Pets.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      I'm in Pets
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
