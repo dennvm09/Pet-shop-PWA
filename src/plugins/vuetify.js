7fb8342e90b992fd5d39625f703ac5bb6c6df6a7
dennvm09 <valenmosquera1999@gmail.com>
2021-02-18 20:12:40 -0500

    learning vue

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify); /** le estoy diciendo a Vue que use vuetify; es como si le estoy dando una dependencia, pa que la tenga en cuenta */
+
+export default new Vuetify({
+});
